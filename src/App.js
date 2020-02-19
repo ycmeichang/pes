225e54d8267649e8e121a97ca758f65764762fb6
meieh <meieh@pm.me>
2020-02-19 19:36:22 +0100

    feat: fetch data and pass data to child component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import Header from './Header'
 import Breeds from './Breeds'
 
 const Container = styled.div`margin: 1.25em;`
 
-export default () => (
-	<Container>
-		<Header />
-		<Breeds />
-	</Container>
-)
+class App extends Component {
+	state = {
+		error: null,
+		isLoaded: false,
+		breeds: []
+	}
+	componentDidMount() {
+		fetch('https://dog.ceo/api/breeds/list/all').then((res) => res.json()).then(
+			(result) => {
+				this.setState({
+					isLoaded: true,
+					breeds: Object.keys(result.message)
+				})
+			},
+			(error) => {
+				this.setState({
+					isLoaded: true,
+					error
+				})
+			}
+		)
+	}
+	render() {
+		const { breeds } = this.state
+		return (
+			<Container>
+				<Header />
+				<Breeds breeds={breeds} />
+			</Container>
+		)
+	}
+}
+
+export default App
