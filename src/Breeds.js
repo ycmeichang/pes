d4c55ac0dc86999575cfbe2622a7e4b88a971023
meieh <meieh@pm.me>
2020-02-21 21:14:48 +0100

    chore: remove unused module

--- src/Breeds.js
+++ src/Breeds.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Container = styled.main`
 	margin: 0 auto;
