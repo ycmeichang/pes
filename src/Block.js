e85b2456515ebd1c0a556868389dcdbf32377d79
meieh <meieh@pm.me>
2020-02-26 20:57:45 +0100

    feat: add cover image to breed list

--- src/Block.js
+++ src/Block.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { Routes } from './routes'
+import { breedList } from './list'
 
 const Container = styled.section`
 	margin: 0 auto;
@@ -15,27 +16,36 @@ const Content = styled.div`
 	flex-basis: 33%;
 	text-align: center;
 	vertical-align: middle;
-	border: 1px solid;
-	margin-right: -1px;
-	margin-bottom: -1px;
 	position: relative;
 	:before {
 		content: '';
 		float: left;
 		padding-top: 100%;
 	}
-	a {
-		margin: 0;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		-ms-transform: translate(-50%, -50%);
-		transform: translate(-50%, -50%);
-	}
 	background-position: center center;
 	background-repeat: no-repeat;
 	background-size: cover;
 	background-image: url(${(props) => props.item});
+`
+const Overlay = styled.div`
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	background: rgba(0, 0, 0, 0.5);
+`
+const BreedLink = styled(Link)`
+	margin: 0;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	-ms-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+	color: white;
+	font-weight: 700;
+	font-size: 18px;
+	text-decoration: none;
 `
 const Title = styled.div`
 	border-bottom: 1px solid #ddd;
@@ -56,8 +66,10 @@ const Block = ({ title, items, type }) => {
 			) : (
 				<Wrap>
 					{items.map((item, index) => (
-						<Content key={index}>
-							<Link to={Routes.showBreedDetail(item)}>{item}</Link>
+						<Content key={index} item={breedList[item]}>
+							<Overlay>
+								<BreedLink to={Routes.showBreedDetail(item)}>{item}</BreedLink>
+							</Overlay>
 						</Content>
 					))}
 				</Wrap>
