5f52b2b9a24d13fad289463bda81c4cbd33f23d0
meieh <meieh@pm.me>
2020-05-29 18:48:47 +0200

    fix bug

--- src/pages/BreedList.js
+++ src/pages/BreedList.js
@@ -4,33 +4,36 @@ import { Container } from '../components/Container'
 import Input from '../components/Input'
 
 const BreedList = () => {
-	const { type } = 'link'
-	const [ search, setSearch ] = useState('')
-	const [ breeds, setBreeds ] = useState([])
-	const [ hasError, setHasError ] = useState(false)
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch('https://dog.ceo/api/breeds/list/all')
-				const json = await response.json()
-				const breeds = Object.keys(json.message)
-				// TODO: sub-breed
-				setBreeds(breeds)
-			} catch (error) {
-				setHasError(error)
-			}
-		}
-		fetchData()
-	}, [])
-	const handleSearch = ({ target: { value } }) => {
-		setSearch(value)
-	}
-	return (
-		<Container>
-			<Input handleSearch={handleSearch} search={search} />
-			<Block items={breeds.filter((breed) => breed.includes(search.toLowerCase()))} type={type} />
-		</Container>
-	)
+  const [type] = useState('link')
+  const [search, setSearch] = useState('')
+  const [breeds, setBreeds] = useState([])
+  const [hasError, setHasError] = useState(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all')
+        const json = await response.json()
+        const breeds = Object.keys(json.message)
+        // TODO: sub-breed
+        setBreeds(breeds)
+      } catch (error) {
+        setHasError(error)
+      }
+    }
+    fetchData()
+  }, [])
+  const handleSearch = ({ target: { value } }) => {
+    setSearch(value)
+  }
+  return (
+    <Container>
+      <Input handleSearch={handleSearch} search={search} />
+      <Block
+        items={breeds.filter((breed) => breed.includes(search.toLowerCase()))}
+        type={type}
+      />
+    </Container>
+  )
 }
 
 export default BreedList
